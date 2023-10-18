8f3af2325010daa2b12ffb7441acc1fc4fc123a6
JesusZaidl <147938485+JesusZaidl@users.noreply.github.com>
2023-10-17 19:56:20 -0600

    Add files via upload

--- validar_usuario.php
+++ validar_usuario.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "phplogin";
+$dbname = "logueo";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,26 +12,18 @@ if ($conn->connect_error) {
     die("La conexión a la base de datos ha fallado: " . $conn->connect_error);
 }
 
-$email = $_POST['email'];
-$password = $_POST['contraseña'];
+$email = $_POST['email']; 
+$password = $_POST['password']; 
 
-$sql = "SELECT * FROM users WHERE Email = '$email' AND Password = '$password'";
-$result = $conn->query($sql);
 
-if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    if ($row['email'] == "@gmail") {
-        $_SESSION['email'] = $email;
-        header("Location: administrador.html");
-    } elseif ($row['email'] !="@gmail") {
-        $_SESSION['email'] = $email;
-        header("Location: principal.html");
-    } else {
-        echo "<script>alert('Tipo de usuario no valido');</script>";
-    }
+if (strpos($email, '@gmail.com') !== false) {
+  
+    $_SESSION['email'] = $email;
+    header("Location: administrador.html");
 } else {
-    echo "<script>alert('Credenciales incorrectas');</script>";
-    
+   
+    $_SESSION['email'] = $email;
+    header("Location: principal.html");
 }
 
 $conn->close();
