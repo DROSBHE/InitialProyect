92457260145b2ffcbd27e94c78b4db3a0aa6ec91
DROSBHE <135923094+DROSBHE@users.noreply.github.com>
2023-10-17 21:47:55 -0600

    Registro de productos

--- addproducto.php
+++ addproducto.php
@@ -13,7 +13,19 @@
         <h1>Agregar productos</h1>
     </header>
   </head>
-  
+  <form action="conRegistro.php" method="post">
+            <input type="number"  name="codigo" id="codigo" placeholder="Ingrese Codigo">
+            <input type="text"  name="descripcion" id="descripcion" placeholder="Ingrese una breve descripcion">
+            <input type="number" min="1"  name="cantidad" id="cantidad" placeholder="Ingrese la cantidad">
+            <select name="estado" id="estado">
+              <option>Disponible</option>
+              <option>En revision</option>
+              <option>Agotado</option>
+            </select>
+            <input type="text" name="responsable" id="responsable" placeholder="Nombre del responsable">
+            
+            <input type="submit" class="botons" value="Guardar" name="btnguardar">
+            <a href="menu.php" class="botons">Regresar</a>
   <body>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
